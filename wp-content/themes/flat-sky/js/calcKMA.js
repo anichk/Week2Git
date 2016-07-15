6620e6c040cd9c786eae80f66fa313491550c9c9
Anichk <anichk@csp.edu>
2016-07-14 23:15:39 -0500

    Additional Commit to js file

--- wp-content/themes/flat-sky/js/calcKMA.js
+++ wp-content/themes/flat-sky/js/calcKMA.js
@@ -1,7 +1,7 @@
 /* calcKMA.js - function used in WordPress theme flat-sky
    Ken Anich - anichk@csp.edu
    Written: July 3, 2016
-   Revised: July 14, 2016 - updating file for GitHub
+   Revised: July 14, 2016 - Update 2 for GitHub
   */
 // Calculate answer for calculator argument
 function calcAnswer() {
